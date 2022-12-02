d32ab94196aec7011db9b48e8878198eaab731a7
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-12-02 17:18:49 +0900

    22-12-02-Ch10-15 순차적 애니메이션 - 순차적으로 요소 보이기

--- js/main.js
+++ js/main.js
@@ -35,3 +35,12 @@ window.addEventListener('scroll', _.throttle(function(){
     }
 }, 300));
 // _.throttle(함수, 시간) : 시간은 밀리세컨드 단위
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(fadeEl, 1, {
+        delay: (index+1) * .7,  // 0.7, 1.4, 2.1, 2.7 순으로 반복
+        opacity: 1,
+        
+    });
+});
