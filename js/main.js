9ad0a8486ee3f4cc682889e3b3cc6eef8242b283
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-12-05 10:59:04 +0900

    22-12-05-Ch10-18 요소 슬라이드 - 수직 슬라이드(Swiper)

--- js/main.js
+++ js/main.js
@@ -44,3 +44,10 @@ fadeEls.forEach(function(fadeEl, index){
         
     });
 });
+
+//new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
